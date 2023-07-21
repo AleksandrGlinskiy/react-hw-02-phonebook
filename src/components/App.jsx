6cd8c7a8aa8d0a9a80ca7f4701bb198714f89379
AleksandrGlinskiy <tarieboe1993@gmail.com>
2023-07-21 16:56:46 +0300

    start doing first step

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,33 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { nanoid } from 'nanoid';
+export class App extends Component {
+  state = {
+    contacts: [],
+    name: '',
+  };
+
+  handleInputChange = e => {
+    this.setState({name: e.target.value});
+  };
+
+  render() {
+    return (
+      <form action="">
+        <label htmlFor="">
+          
+          Name
+          <input
+            type="text"
+            name="name"
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            required
+            value={this.state.name}
+            onChange={this.handleInputChange}
+          />
+        </label>
+        <button type='submit'>Add contact</button>
+      </form>
+    );
+  }
+}
